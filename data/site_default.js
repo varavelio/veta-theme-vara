d5eb010fbfeffa84fcc79adadd4b319bacafab58
Luis Eduardo <eduardo.devop@gmail.com>
2026-07-03 10:10:42 -0600

    chore(site): add default site configuration

---
+++ data/site_default.js
@@ -0,0 +1,15 @@
+export default function() {
+  return {
+    title: "VaraPress",
+    description: "A clean, fast, and responsive all-in-one Veta theme for Varavel projects",
+    favicon: "https://cdn.jsdelivr.net/gh/varavelio/brand@v1.0.2/dist/avatar-dark.svg",
+    logo_light: "https://cdn.jsdelivr.net/gh/varavelio/brand@v1.0.2/dist/logo-black.svg",
+    logo_dark: "https://cdn.jsdelivr.net/gh/varavelio/brand@v1.0.2/dist/logo-white.svg",
+    show_title: true,
+    github_repo: "varavelio/varapress",
+    sidebar_links: [],
+    llms: false,
+    llms_full: false,
+    year: new Date().getFullYear(),
+  };
+}
