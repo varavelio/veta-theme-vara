a167b8a03a3816b05b100938d22ef8089410ec5b
Luis Eduardo <eduardo.devop@gmail.com>
2026-07-03 09:56:53 -0600

    chore(icons): normalize and minify downloaded SVGs

--- scripts/download-icons.sh
+++ scripts/download-icons.sh
@@ -29,5 +29,28 @@ mv -f $TMP_DIR/lucide/icons/*.svg $ICONS_DIR
 rm -rf $ICONS_DIR/varavel.svg
 wget -q -O $ICONS_DIR/varavel.svg https://raw.githubusercontent.com/varavelio/brand/refs/tags/v1.0.2/dist/logo-black.svg
 
+# Normalize SVGs for template usage
+for icon in "$ICONS_DIR"/*.svg; do
+  # Detect if is solid or bordered
+  if grep -q 'fill="none"' "$icon"; then
+    fill='fill="none"'
+    stroke='stroke="currentColor"'
+  else
+    fill='fill="currentColor"'
+    stroke='stroke="none"'
+  fi
+
+  # Normalize SVG
+  sed -i -z 's/xmlns="[^"]*"//g' "$icon" # Remove xmlns
+  sed -i -z 's/class="[^"]*"//g' "$icon" # Remove class
+  sed -i -z 's/fill="[^"]*"//g' "$icon" # Remove fill
+  sed -i -z 's/stroke="[^"]*"//g' "$icon" # Remove stroke
+  sed -i -z 's/<title>[^<]*<\/title>//g' "$icon" # Remove title
+  sed -i -z "s/<svg/<svg $fill $stroke/" "$icon" # Add the new fill and stroke
+done
+
+# Minify SVGs
+svgo -rf "$ICONS_DIR" --quiet --multipass --precision=3
+
 # Clean up temporary directory
 rm -rf $TMP_DIR
